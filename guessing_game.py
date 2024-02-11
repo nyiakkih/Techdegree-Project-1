507874e52e8ec4d419ee79739a787cbb7093e60a
nyiakkih <159721580+nyiakkih@users.noreply.github.com>
2024-02-11 18:02:00 -0500

    Removed all comments

--- guessing_game.py
+++ guessing_game.py
@@ -4,7 +4,6 @@ Project 1 - A Number Guessing Game
 --------------------------------
 """
 
-# Import the random and statistics modules.
 import random
 from statistics import mean, median, mode
 
@@ -21,8 +20,7 @@ def stats():
 def highscore():
     score_min = min(scoreboard)
     print(f"Highscore: {score_min}")
-# Create the start_game function.
-# Write your code inside this function.
+
 def intro():
     print("Welcome to \"Can You Guess the Number?\"")
 
@@ -31,14 +29,6 @@ def start_game():
     answer = random.randint(1, 100)
     attempts = 0
 
-#   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
-#   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
     while True:
         try:
           guess = int(input("Enter your guess: "))
@@ -62,14 +52,7 @@ def start_game():
 
     print(f"It took you {attempts} attempts.")
     scoreboard.append(attempts)
-    #   5. Display the following data to the player
-    #     a. How many attempts it took them to get the correct number in this game
-    #     b. The mean of the saved attempts list
-    #     c. The median of the saved attempts list
-    #     d. The mode of the saved attempts list
-    #   6. Prompt the player to play again
-    #     a. If they decide to play again, start the game loop over.
-    #     b. If they decide to quit, show them a goodbye message.
+
     new_game = input("Would you like to play again? (Y/N)  ").upper()
     if new_game == "Y":
         highscore()    
@@ -77,12 +60,6 @@ def start_game():
     elif new_game == "N":
         print("Thank you for playing!")
         stats()   
-    
 
 intro()
 start_game()
-
-# ( You can add more features/enhancements if you'd like to. )
-
-
-# Kick off the program by calling the start_game function.
